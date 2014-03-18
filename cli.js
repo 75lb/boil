fa9a2ab50e9c0bb67d1ab99d679ee1e772712281
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-03-18 16:56:31 +0100

    pass in config

--- cli.js
+++ cli.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 "use strict";
 
-var boil = require("./lib/boil");
+var boil = require("./lib/boil"),
+    config = require("./config.json");
 
 var target = process.argv[2];
 
 if (target){
-    boil(target);
+    boil(config, target);
 } else {
     console.log("Need a target");
 }
