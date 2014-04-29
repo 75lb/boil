7f920d2c6afb234ea7c185a1b5814297889fd753
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-04-29 15:10:57 +0100

    load-config and updated cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
 "use strict";
 
-var boil = require("./lib/boil"),
-    config = require("./config.json");
+var boil = require("../lib/boil"),
+    loadConfig = require("load-config"),
+    path = require("path"),
+    w = require("wodge");
 
 var target = process.argv[2];
 
 if (target){
-    boil(config, target);
+    var config = loadConfig(
+        path.join(w.getHomeDir(), ".boil.json"),
+        path.join(process.cwd(), "config.json"),
+        path.join(process.cwd(), "package.json:boil")
+    );
+    boil.boil(config, target);
 } else {
     console.log("Need a target");
 }
