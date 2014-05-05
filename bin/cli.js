373214b00d62eeaa6df3db7ffaf2258f3ca2a9fb
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-05-05 15:32:49 +0100

    wip

--- bin/cli.js
+++ bin/cli.js
@@ -2,15 +2,31 @@
 "use strict";
 
 var boil = require("../lib/boil"),
-    loadConfig = require("load-config"),
+    loadConfig = require("config-master"),
     path = require("path"),
-    w = require("wodge");
+    w = require("wodge"),
+    Model = require("nature").Model;
 
-var target = process.argv[2];
+var usage = "Usage: \nboil <recipes>";
+
+var argv = new Model()
+    .define({ name: "help", alias: "h", type: "boolean" })
+    .define({ name: "recipe", alias: "r", type: Array, defaultOption: true })
+    .set(process.argv);
+
+if (argv.help) {
+    console.log(usage);
+    process.exit(0);
+}
 
 var config = loadConfig(
     path.join(w.getHomeDir(), ".boil.json"),
     path.join(process.cwd(), "boil.json"),
     path.join(process.cwd(), "package.json:boil")
 );
-boil.boil(config, target);
+
+if (argv.recipe) {
+    argv.recipe.forEach(boil.boil.bind(null, config));
+} else {
+    boil.boil(config);
+}
