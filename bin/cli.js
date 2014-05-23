4d82f40302206dffb9055d3269dc97a08c00fa15
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-05-23 18:05:34 +0200

    bugs

--- bin/cli.js
+++ bin/cli.js
@@ -25,33 +25,41 @@ if (argv.help) {
     process.exit(0);
 }
 
-/* load boil config data */
-var config = loadConfig(
-    path.join(w.getHomeDir(), ".boil.json"),
-    path.join(process.cwd(), "boil.json"),
-    path.join(process.cwd(), "package.json:boil")
-);
 
-if (Object.keys(config).length === 0){
-    var boilHbsPath = path.join(process.cwd(), "boil.hbs");
-    if (fs.existsSync(boilHbsPath)){
-        var boilHbs = mfs.read(boilHbsPath);
-        config = JSON.parse(boil.render(boilHbs));
+function getConfig(){
+    /* load boil config data */
+    var config = loadConfig(
+        path.join(w.getHomeDir(), ".boil.json"),
+        path.join(process.cwd(), "boil.json"),
+        path.join(process.cwd(), "package.json:boil")
+    );
+
+    /* this was failing when running -t boil.hbs -d copy.json */
+    // if (Object.keys(config).length === 0){
+    //     var boilHbsPath = path.join(process.cwd(), "boil.hbs");
+    //     if (fs.existsSync(boilHbsPath)){
+    //         var boilHbs = mfs.read(boilHbsPath);
+    //         config = JSON.parse(boil.render(boilHbs));
+    //     }
+    // }
+
+    if (Object.keys(config).length === 0){
+        console.error("No config");
+        process.exit(1);
     }
+    
+    return config;
 }
 
-if (Object.keys(config).length === 0){
-    console.error("No config");
-    process.exit(1);
+if (argv.config){
+    console.dir(getConfig());
+    process.exit(0);
 }
 
 if (argv.helper) argv.helper.forEach(boil.registerHelpers);
 
-if (argv.config){
-    console.dir(config);
-    process.exit(0);
-
-} else if (argv.recipe && argv.recipe.length) {
+if (argv.recipe && argv.recipe.length) {
+    var config = getConfig();
     argv.recipe.forEach(boil.renderRecipe.bind(null, config));
 
 } else if (argv.template) {
