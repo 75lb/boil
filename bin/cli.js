5db88b8bf4b34f9aeba77349d6eadf9294ffebe5
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-08-10 19:51:41 +0200

    removed setArgs.. now accepted cwd/boil.hbs..

--- bin/cli.js
+++ bin/cli.js
@@ -2,22 +2,23 @@
 "use strict";
 
 var boil = require("../"),
-    loadConfig = require("config-master"),
+    mergeConfig = require("config-master"),
     dope = require("console-dope"),
     path = require("path"),
     mfs = require("more-fs"),
     fs = require("fs"),
     cliArgs = require("command-line-args"),
-    getHomeDir = require("home-path");
+    getHomeDir = require("home-path"),
+    o = require("object-tools");
 
 /* Define command-line args  */
 var cli = cliArgs([
     { name: "help", alias: "h", type: Boolean },
-    { name: "recipe", alias: "r", type: Array, defaultOption: true },
+    { name: "recipes", alias: "r", type: Array, defaultOption: true },
     { name: "config", alias: "c", type: Boolean },
     { name: "list", alias: "l", type: Boolean },
-    { name: "helper", alias: "f", type: Array },
-    { name: "partial", alias: "p", type: Array },
+    { name: "helpers", alias: "f", type: Array },
+    { name: "partials", alias: "p", type: Array },
     { name: "template", alias: "t", type: String },
     { name: "data", alias: "d", type: String },
     { name: "args", alias: "a", type: Array }
@@ -40,18 +41,44 @@ if (argv.help) {
     process.exit(0);
 }
 
+/* returns an object containing boil options */
 function getConfig(){
-    /* load boil config data */
-    var config = loadConfig(
-        path.join(getHomeDir(), ".boil.json"),
-        path.join(process.cwd(), "boil.json"),
-        { jsonPath: path.join(process.cwd(), "package.json"), configProperty: "boil" }
+    /* pertinent paths */
+    var p = {
+        cwd: path.join(process.cwd(), "boil.json"),
+        cwdHbs: path.join(process.cwd(), "boil.hbs"),
+        pkg: path.join(process.cwd(), "package.json"),
+        home: path.join(getHomeDir(), ".boil.json"),
+        boilDir: path.join(getHomeDir(), ".boil")
+    };
+    
+    if (!fs.existsSync(p.cwd) && fs.existsSync(p.cwdHbs)){
+        var tmpPath = mfs.getTempFilePath("boilHbs.json");
+        var result = boil.render(mfs.read(p.cwdHbs));
+        // console.log(result)
+        fs.writeFileSync(tmpPath, result);
+    }
+    
+    /*
+    merge config from these locations, in this order:
+    ~/.boil.json
+    $(pwd)/boil.json
+    $(pwd)/package.json ("boil" property)
+    */
+    var config = mergeConfig(
+        p.home,
+        tmpPath || p.cwd,
+        { jsonPath: p.pkg, configProperty: "boil" }
     );
 
-    var boilDir = path.join(getHomeDir(), ".boil");
-    var recipeFiles = fs.readdirSync(boilDir);
+    mfs.deleteFile(tmpPath);
+    
+    /*
+    add each file in ~/.boil as a recipe
+    */
+    var recipeFiles = fs.readdirSync(p.boilDir);
     recipeFiles.forEach(function(file){
-        var template = fs.readFileSync(path.join(boilDir, file), "utf8");
+        var template = fs.readFileSync(path.join(p.boilDir, file), "utf8");
         config[path.basename(file, ".hbs")] = {
             template: template
         };
@@ -77,10 +104,9 @@ if (argv.list){
 if (argv.helper) boil.registerHelpers(argv.helper);
 if (argv.partial) boil.registerPartials(argv.partial);
 
-if (argv.recipe && argv.recipe.length) {
+if (argv.recipes && argv.recipes.length) {
     var config = getConfig();
-    config = boil.setArgs(config, argv.args)
-    argv.recipe.forEach(boil.renderRecipe.bind(null, config));
+    argv.recipes.forEach(boil.renderRecipe.bind(null, config, argv.args));
 
 } else if (argv.template) {
     var data = argv.data
